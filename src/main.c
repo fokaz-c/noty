0b9f538d49078172d6f0b02a6b583abf501b8219
fokaz-c <fokazmchakma4427@gmail.com>
2025-09-19 13:57:41 +0530

    feat(terminal): add raw mode with error handling
    - Enable raw mode
    - Disable echoing
    - Disable canonical mode
    - Disable all control signals
    - Display keypresses
    - Restore terminal state on exit
    - Add error handling function

--- src/main.c
+++ src/main.c
@@ -1,37 +1,61 @@
+/***    includes    ***/
+#include <asm-generic/errno-base.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 #include <ctype.h>
 
+/***    data    ***/
 struct termios original_termios;
 
+/***    terminal    ***/
+void noty_kill(const char *source) {
+    perror(source);
+    exit(1);
+}
+
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1)
+        noty_kill("tcsetattr");
 }
 
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &original_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1)
+        noty_kill("tcsetattr");
     atexit(disableRawMode);
 
     struct termios raw = original_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_iflag &= ~(BRKINT |ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) 
+        noty_kill("tcsetattr");
 }
 
-
+/***    init/main   ***/
 int main() {
     enableRawMode();
     char c;
-    while (read(STDIN_FILENO, &c , 1) == 1 && c != 'q') {
+    read(STDIN_FILENO, &c , 1); 
+    while (1) {
+        char c;
+        if(read(STDIN_FILENO, &c , 1) == -1 && errno != EAGAIN)
+            noty_kill("Read");
         if (iscntrl(c)) {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }
         else{
-            printf("%d (%c)\n",c,c);
+            printf("%d (%c)\r\n",c,c);
         }
+        if (c == 'q') break;
     }
     printf("yeah compiles.\n");
     return 0;
